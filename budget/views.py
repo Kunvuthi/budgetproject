c16dd308c6040827b158ba8dd2a46bc6000f5283
Kunvuthi <43140224+Kunvuthi@users.noreply.github.com>
2019-08-21 19:59:12 +0700

    Initial commit

---
+++ budget/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def project_list(request):
+    return render(request, 'budget/project-lists.html')
+
+def project_detail(request, project_slug):
+    # fetching the correct project 
+    return render(request, 'budget/project-detail.html')
