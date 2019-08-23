b23302f577794e5405def96e13959f222fe8fe9c
Kunvuthi <43140224+Kunvuthi@users.noreply.github.com>
2019-08-23 15:02:04 +0700

    model commit#3

--- budget/views.py
+++ budget/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project
+
 
 # Create your views here.
 def project_list(request):
     return render(request, 'budget/project-lists.html')
 
 def project_detail(request, project_slug):
-    # fetching the correct project 
-    return render(request, 'budget/project-detail.html')
+    project = get_object_or_404(Project, slug=project_slug)
+    return render(request, 'budget/project-detail.html', {'project': project, 'expense_list': project.expenses.all()})
