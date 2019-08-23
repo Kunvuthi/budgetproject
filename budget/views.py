4886fa83e6263bc14832624ec24b5d3b8c02d807
Kunvuthi <43140224+Kunvuthi@users.noreply.github.com>
2019-08-23 15:29:14 +0700

    add-project commit#4

--- budget/views.py
+++ budget/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Project
+from django.views.generic import CreateView
 
 
 # Create your views here.
@@ -9,3 +10,10 @@ def project_list(request):
 def project_detail(request, project_slug):
     project = get_object_or_404(Project, slug=project_slug)
     return render(request, 'budget/project-detail.html', {'project': project, 'expense_list': project.expenses.all()})
+
+
+
+class ProjectCreateView(CreateView):
+    model = Project 
+    template_name = 'budget/add-project.html'
+    fields = ('name', 'budget')
